2dea8f892b5722eaf9889beebe69fa506c60b405
Krishiv Khatri <krishivkhatri2409@gmail.com>
2025-04-23 20:54:04 -0400

    fixed old supabase deps in all files

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,7 +5,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const createClient = async () => {
-  const cookieStore = cookies()
+  const cookieStore = await cookies()
   return createServerClient(supabaseUrl, supabaseAnonKey, {
     cookies: {
       get(name: string) {
